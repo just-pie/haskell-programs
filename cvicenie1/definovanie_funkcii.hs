b446d1ed58c3b7a40655e24d7c9f35e4f932e225
just-pie <stefi7773@gmail.com>
2020-03-19 17:36:35 +0000

    Build and Clean

---
+++ cvicenie1/definovanie_funkcii.hs
@@ -0,0 +1,32 @@
+absolutna x = if x < 0 then x*(-1) else x
+
+absolutnaGE x | x < 0 = x*(-1)
+              | otherwise = x
+
+znamienko x = if x < 0 then -1 else if x == 0 then 0 else 1
+
+znamienkoGE x | x < 0 = -1
+              | x == 0 = 0
+              | otherwise = 1
+
+mocnina a n = if n == 0 then 1 else a * mocnina a (n-1)
+
+mocninaGE a n | n == 0 = 1
+              | a == 0 = 1      
+              | n < 0 = a * mocninaGE a (absolutnaGE n)
+              | otherwise = a * mocninaGE a (n-1)
+
+mocninaLogGE a n | n == 0 = 1
+                 | n `mod` 2 == 1 = a * mocninaLogGE a (n-1)
+                 | otherwise = mocninaLogGE (a*a) (div n 2)
+
+urocenaSuma suma urok roky | roky == 0 = suma
+                           | urok == 0 = suma
+                           | suma == 0 = 0
+                           | otherwise = urocenaSuma (suma+((suma/100)*urok)) urok (roky-1)
+{-
+main = do
+  print(urocenaSuma 100 10 10)
+  -- return
+-}
+  
---
+++ cvicenie2/distancne_zadanie.hs
@@ -0,0 +1,5 @@
+porovnaj_cas (h1,m1,s1,h2,m2,s2) | h1 == h2 && m1 == m2 && s1 == s2 = "Su rovnake"
+
+
+main = do
+  print(porovnaj_cas(15,20,30,15,20,30))
