f223813937870a2ad2fabcf07c26aa338ff4c273
pajka-js <26172627+pajka-js@users.noreply.github.com>
2020-04-09 08:18:36 +0200

    Added tupleList

--- cvicenie3/funkcie_vyssieho_radu.hs
+++ cvicenie3/funkcie_vyssieho_radu.hs
@@ -1,1 +1,6 @@
+tupleList [] = []
+tupleList (x:xs) | even x == True = (x, True):(tupleList xs)
+                 | otherwise = (x, False):(tupleList xs)
 
+main = do
+  print(tupleList[1,3,2,-1,7,0])
