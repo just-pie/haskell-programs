39c8ca7cef5d727291b4d5dd3e0eedbffa0fc323
pajka-js <26172627+pajka-js@users.noreply.github.com>
2020-04-09 08:31:48 +0200

    Update funkcie_vyssieho_radu.hs

--- cvicenie3/funkcie_vyssieho_radu.hs
+++ cvicenie3/funkcie_vyssieho_radu.hs
@@ -2,5 +2,7 @@ tupleList [] = []
 tupleList (x:xs) | even x == True = (x, True):(tupleList xs)
                  | otherwise = (x, False):(tupleList xs)
 -- tupleList(x:xs) = (x, even x):(tupleList xs) -- Da sa aj takto jednoducho
+tupleListMap xs = map (\x -> (x, even x)) xs
 main = do
   print(tupleList[1,3,2,-1,7,0])
+  print(tupleListMap[1,3,2,-1,7,0])
