88c3778a4d54715db4906a4309f6e27b1d773991
pajka-js <26172627+pajka-js@users.noreply.github.com>
2020-04-09 08:46:29 +0200

    Update funkcie_vyssieho_radu.hs

--- cvicenie3/funkcie_vyssieho_radu.hs
+++ cvicenie3/funkcie_vyssieho_radu.hs
@@ -8,7 +8,12 @@ tupleListMap xs = map (\x -> (x, even x)) xs
 
 -- 2. Mucha
 -- mucha :: Char -> String
-mucha c = map (\x -> if any (==x) ['a','e','i','o','u','y'] then c else x) "Sedi mucha na stene"
+-- mucha c = map (\x -> if any (==x) ['a','e','i','o','u','y'] then c else x) "Sedi mucha na stene"
+-- Tu zistujem, ci je na vstupe samohlaska a ak nie, tak vypise error 
+mucha c = if any (==c) ['a','e','i','o','u','y']
+          then map (\x -> if any (==x) ['a','e','i','o','u','y'] 
+                          then c else x) "Sedi mucha na stene"
+          else error "Zadany znak nie je samohlaska"
 
 main = do
   print(tupleList[1,3,2,-1,7,0])
