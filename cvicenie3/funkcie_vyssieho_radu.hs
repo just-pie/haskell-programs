70d318fc7bb49a465640c4109c61b2b11bc4025f
Kristína Šteflovičová <26172627+pajka-js@users.noreply.github.com>
2020-04-16 08:07:38 +0200

    Update funkcie_vyssieho_radu.hs

--- cvicenie3/funkcie_vyssieho_radu.hs
+++ cvicenie3/funkcie_vyssieho_radu.hs
@@ -6,6 +6,7 @@ tupleList (x:xs) | even x == True = (x, True):(tupleList xs)
 -- tupleList(x:xs) = (x, even x):(tupleList xs) -- Da sa aj takto jednoducho
 tupleListMap xs = map (\x -> (x, even x)) xs
 
+-- ===================================================================
 -- 2. Mucha
 -- mucha :: Char -> String
 -- mucha c = map (\x -> if any (==x) ['a','e','i','o','u','y'] then c else x) "Sedi mucha na stene"
@@ -20,7 +21,7 @@ mucha c = if elem c "aeiouy"
                           then c else x) "Sedi mucha na stene"
           else error "Zadany znak nie je samohlaska"
 -}
-
+-- ===================================================================
 -- 3. mini :: Ord a => [a] -> a
 -- Nájde minimum v zadanom zozname prvkov.
 -- acc = akumulator na docasny vysledok
@@ -37,6 +38,14 @@ miniFR (x:xs) = foldr (\x acc -> if x < acc then x else acc) x xs
 -- scanr pre vypis sprava
 miniSCR (x:xs) = scanr (\x acc -> if x < acc then x else acc) x xs
 
+-- ===================================================================
+-- 4. Funkcia prvok :: Eq a => a -> [a] -> Bool zistí, či sa daný prvok nachádza v zozname.
+prvok _ [] = False
+prvok x (y:ys) | x == y = True
+               | otherwise = prvok x(ys)
+
+prvokFR a xs = foldr (\x acc -> if x == a then True else acc) False xs
+
 main = do
   print(tupleList[1,3,2,-1,7,0])
   print(tupleListMap[1,3,2,-1,7,0])
@@ -44,3 +53,5 @@ main = do
   print(miniSCL([1,2,3,6,-5]))
   print(miniFR([1,2,3,6,-5]))
   print(miniSCR([1,2,3,6,-5]))
+  print(prvok 5 [1,54,8,45,6])
+  print(prvokFR 5 [1,54,8,45,6])
