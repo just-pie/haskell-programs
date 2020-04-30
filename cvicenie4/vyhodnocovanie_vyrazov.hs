eb8323eb6340f96f0471cea5d4daec6091016b68
Kristína Šteflovičová <26172627+pajka-js@users.noreply.github.com>
2020-04-30 08:25:44 +0200

    reverse using foldr

--- cvicenie4/vyhodnocovanie_vyrazov.hs
+++ cvicenie4/vyhodnocovanie_vyrazov.hs
@@ -9,7 +9,14 @@ mapl f (xs)   = foldl (\acc x -> acc++[(f x)]) [] xs
 -- head (filterr (>5) [1..])
 filterr f xs = foldr (\ x acc -> if f x then x:acc else acc) [] xs
 -- head (filterl (>5) [1..])
-filterl f xs = foldl (\acc x -> if f x then acc ++ [x] else acc) []
+filterl f xs = foldl (\acc x -> if f x then acc ++ [x] else acc) [] xs
+
+------------------------------------------------------------------
+-- 3. Naprogramujte funkciu reverse pomocou funkcií foldr a foldl. Funkcie nazvite napr. revr, revl.
+revr xs = foldr (\ x acc -> acc ++ [x]) [] xs
+
+
+
 
 main = do
   print(mapr (+1) [1,2,3])          -- [2,3,4]
@@ -20,4 +27,4 @@ main = do
   print(filterl (>5) [1..10])       -- [6,7,8,9,10]
   print(head (filterr (>5) [1..]))  -- 6
   print(head (filterl (>5) [1..]))  -- 6
-  
+  print(revr [1..10])               -- [10,9,8,7,6,5,4,3,2,1]
