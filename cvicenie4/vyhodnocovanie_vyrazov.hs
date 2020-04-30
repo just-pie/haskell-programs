d695db2835d305cf7766c1d6bd4ad386d8b78101
Kristína Šteflovičová <26172627+pajka-js@users.noreply.github.com>
2020-04-30 08:10:59 +0200

    Update vyhodnocovanie_vyrazov.hs

--- cvicenie4/vyhodnocovanie_vyrazov.hs
+++ cvicenie4/vyhodnocovanie_vyrazov.hs
@@ -4,8 +4,20 @@ mapr f xs = foldr (\x acc -> (f x):acc) [] xs
 -- take 3 (mapl (+1) [1..])
 mapl f (xs)   = foldl (\acc x -> acc++[(f x)]) [] xs
 
+------------------------------------------------------------------
+-- 2. Naprogramujte funkciu filter pomocou funkcií foldr a foldl. Funkcie nazvite napr. filterr, filterl. Vyskúšajte filtrovanie pre nekonečný zoznam:
+-- head (filterr (>5) [1..])
+filterr f xs = foldr (\ x acc -> if f x then x:acc else acc) [] xs
+-- head (filterl (>5) [1..])
+filterl f xs = foldl (\acc x -> if f x then acc ++ [x] else acc) []
+
 main = do
   print(mapr (+1) [1,2,3])          -- [2,3,4]
   print(mapl (+1) [1,2,3])          -- [2,3,4]
   print(take 3 (mapr (+1) [1..]))   -- prints first 3 numbers of infinite list
   print([1..1000])                  -- prints numbers from 1 to 1000
+  print(filterr (>5) [1..10])       -- [6,7,8,9,10]
+  print(filterl (>5) [1..10])       -- [6,7,8,9,10]
+  print(head (filterr (>5) [1..]))  -- 6
+  print(head (filterl (>5) [1..]))  -- 6
+  
