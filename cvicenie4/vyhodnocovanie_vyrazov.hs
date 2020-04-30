e5201aa426081ec517bb51e1babb7b86be2f5187
Kristína Šteflovičová <26172627+pajka-js@users.noreply.github.com>
2020-04-30 07:54:57 +0200

    Update vyhodnocovanie_vyrazov.hs

--- cvicenie4/vyhodnocovanie_vyrazov.hs
+++ cvicenie4/vyhodnocovanie_vyrazov.hs
@@ -7,4 +7,5 @@ mapl f (xs)   = foldl (\acc x -> acc++[(f x)]) [] xs
 main = do
   print(mapr (+1) [1,2,3])          -- [2,3,4]
   print(mapl (+1) [1,2,3])          -- [2,3,4]
+  print(take 3 (mapr (+1) [1..]))   -- prints first 3 numbers of infinite list
   print([1..1000])                  -- prints numbers from 1 to 1000
