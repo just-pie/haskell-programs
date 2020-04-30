73e30597e68cbd29ef57f772223460cd21008641
Kristína Šteflovičová <26172627+pajka-js@users.noreply.github.com>
2020-04-30 07:51:08 +0200

    foldr and fold to map

--- cvicenie4/vyhodnocovanie_vyrazov.hs
+++ cvicenie4/vyhodnocovanie_vyrazov.hs
@@ -1,1 +1,9 @@
+-- 1. Naprogramujte funkciu map pomocou funkcií foldr a foldl. Funkcie nazvite napr. mapr, mapl. Vyskúšajte mapovanie pre nekonečný zoznam:
+-- take 3 (mapr (+1) [1..])
+mapr f xs = foldr (\x acc -> (f x):acc) [] xs
+-- take 3 (mapl (+1) [1..])
+mapl f (xs)   = foldl (\acc x -> acc++[(f x)]) [] xs
 
+main = do
+  print(mapr (+1) [1,2,3])          -- [2,3,4]
+  print(mapl (+1) [1,2,3])          -- [2,3,4]
