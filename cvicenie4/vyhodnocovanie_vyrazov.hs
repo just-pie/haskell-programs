19d1c4cf6b35887ba45d9155d48e1a33934eb589
Kristína Šteflovičová <26172627+pajka-js@users.noreply.github.com>
2020-05-07 08:25:45 +0200

    Update vyhodnocovanie_vyrazov.hs

--- cvicenie4/vyhodnocovanie_vyrazov.hs
+++ cvicenie4/vyhodnocovanie_vyrazov.hs
@@ -39,6 +39,42 @@ sucet [1,2,3] =
 sumTR [] acc = acc
 sumTR (x:xs) acc = sumTR xs x+acc
 
+------------------------------------------------------------------
+-- 5. Naprogramujte funkciu pocet :: Num a => [a] -> Int na výpočet počtu prvkov v zozname pomocou rekurzie a pattern matchingu. Upravte ju na funkciu s chvostovou rekurziou.
+pocet [] = 0
+pocet (x:xs) = 1 + pocet xs
+
+-- Using Tail recursion
+pocetTR [] acc = acc
+pocetTR (x:xs) acc = pocetTR xs acc + 1
+-- vramci parametrov zadam funkciu
+
+-- How does it work?
+{-
+pocet [1,2,3] = 1 + pocet [2,3] =
+              = 1 + (1 + pocet [3]) =
+              = 1 + (1 + (1 + pocet [])) = 
+              = 1 + (1 + (1 + 0)) =
+              = 1 + (1 + 1) = 
+              = 1 + 2 = 
+              = 3
+-}
+
+{-
+pocetTR [1,2,3] 0 = pocetTR [2,3] 1 =
+                  = pocetTR [3] 2 =
+                  = pocetTR [] 2+1 = 
+                  = 3
+-}
+
+------------------------------------------------------------------
+-- 6. Naprogramujte funkciu sucpoc :: Num a => [a] -> (a,Int) -> (a,Int), ktorej výsledkom bude dvojica súčet a počet prvkov zoznamu. Funkcia bude chvostovo rekurzívna s parametrami zoznam a dvojica s akumulovaným výsledkom.
+-- sucpoc [1,2,3] (0,0) = (6,3)
+
+sucpos [] (accS, accP) = (accS, accP)
+sucpos (x:xs) (accS, accP) = sucpos xs (accS+x, accP+1)
+sucpoc2 xs = sucpos xs (0,0) -- toto nam zjednodusuje print
+
 main = do
   print(mapr (+1) [1,2,3])          -- [2,3,4]
   print(mapl (+1) [1,2,3])          -- [2,3,4]
@@ -52,3 +88,6 @@ main = do
   print(revl [1..10])               -- [10,9,8,7,6,5,4,3,2,1]
   print(sucet [1,2,3])              -- 6
   print(sumTR [1..3] 0)             -- 6 (Tail recursion)
+  print(pocet [1..10])              -- 10
+  print(pocetTR [1..10] 0)          -- 10 (Tail recursion)
+  print(sucpoc2 [1..10])            -- (55,10)
